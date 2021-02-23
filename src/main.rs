1912df7e83c191599d4dc422a346b28d7a29dab0
Gary Moore <madmangaz@gmail.com>
2021-02-23 20:31:14 +0000

    Update .gitignore and add Cargo.lock to scm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::net::TcpListener;
+use std::thread::spawn;
+use tungstenite::{accept, Error, Message};
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind("0.0.0.0:8765").unwrap();
+    for stream in server.incoming() {
+        spawn(move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+            loop {
+                let processed_msg = match websocket.read_message() {
+                    Ok(msg) => {
+                        let msg = msg.to_string();
+                        let split_msg: Vec<&str> = msg.split(',').collect();
+                        format!(
+                            "x: {}\ty: {}\tz: {}",
+                            split_msg[0], split_msg[1], split_msg[2]
+                        )
+                    },
+                    Err(_) => {
+                        String::from("Connection failed")
+                    }
+                };
+
+                println!("{}", processed_msg)
+            }
+        });
+    }
 }
